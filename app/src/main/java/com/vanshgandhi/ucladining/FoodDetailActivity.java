b2abdef07000d30e8e0341b21faeb9ed0bd62e30
Vansh Gandhi <vansh.gandhi@gmail.com>
2015-11-02 20:52:57 -0800

    Formatted Nutrition Facts Table

--- app/src/main/java/com/vanshgandhi/ucladining/FoodDetailActivity.java
+++ app/src/main/java/com/vanshgandhi/ucladining/FoodDetailActivity.java
@@ -28,7 +28,10 @@ public class FoodDetailActivity extends AppCompatActivity
         setContentView(R.layout.activity_food_detail);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Name of Food");
         setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener()
@@ -36,10 +39,10 @@ public class FoodDetailActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Saved to Favorites", Snackbar.LENGTH_LONG).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
