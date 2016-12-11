a7a31b642d608030826391869f81a995380f6515
Sahil Gandhi <sahilmgandhi@gmail.com>
2016-12-10 22:24:01 -0800

    Added some date picker code

--- BruinMenu/app/src/main/java/com/vanshgandhi/bruinmenu/MainActivity.java
+++ BruinMenu/app/src/main/java/com/vanshgandhi/bruinmenu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vanshgandhi.bruinmenu;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.TabLayout;
@@ -8,14 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mDay, mMonth, mYear;
 
     @BindView (R.id.container) ViewPager viewPager;
     @BindView (R.id.tabs) TabLayout tabLayout;
@@ -52,8 +59,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        mMonth = c.get(Calendar.MONTH);
+
         if (id == R.id.action_calendar) {
             //TODO: Show date picker
+            DatePickerDialog dpd = new DatePickerDialog(MainActivity.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int day) {
+                            c.set(year, month, day);
+
+                            mYear = c.get(Calendar.YEAR);
+                            mMonth = c.get(Calendar.MONTH);
+                            mDay = c.get(Calendar.DAY_OF_MONTH);
+                        }
+                    }, mYear, mMonth, mDay);
+            dpd.getDatePicker().setMinDate(System.currentTimeMillis());
+
+            Calendar d = Calendar.getInstance();
+            d.add(Calendar.MONTH,1);
+
+            dpd.getDatePicker().setMaxDate(d.getTimeInMillis());
+            dpd.show();
             return true;
         }
         return super.onOptionsItemSelected(item);
