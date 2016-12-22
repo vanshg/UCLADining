2cf18bb505b6999955fade3fded71e77e273d2d5
Jonathan Zaturensky <jzaturensky@g.ucla.edu>
2016-12-21 22:16:33 -0800

    added /menus route with parsed response

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,8 @@ var request = require('request')
 var util = require('util')
 var cheerio = require('cheerio')
 var app = express()
+var tabletojson = require('tabletojson');
+var fs = require('fs');
 
 let hoursUrl = 'https://secure5.ha.ucla.edu/restauranthours/dining-hall-hours-by-day.cfm?serviceDate=%d%%2F%d%%2F%d'
 let overviewUrl = 'http://menu.ha.ucla.edu/foodpro/default.asp?date=%d%%2F%d%%2F%d'
@@ -50,6 +52,16 @@ app.get('/hours', function (req, res) {
         }
     })
 })
+
+/* Parameters:
+    Date
+*/
+app.get('/menus', function (req, res) {
+    // temporary cache file since website is down
+    var html = fs.readFileSync("test.html");
+    parseMenus(res, html);
+})
+
 
 /* Parameters:
     Date
@@ -107,6 +119,142 @@ function parseHours(res, body) {
     res.send(response)
 }
 
+function parseMenus(res, html)
+{
+    var response = 
+    {
+        "Breakfast" : 
+        [
+            {
+                "name" : "Bruin Plate",
+                "menu" : []
+            },
+            {
+                "name" : "De Neve Dining",
+                "menu" : []
+            }
+        ],
+        "Lunch" : 
+        [
+            {
+                "name" : "Covel Dining",
+                "menu" : []
+            },
+            {
+                "name" : "Bruin Plate",
+                "menu" : []
+            },
+            {
+                "name" : "De Neve Dining",
+                "menu" : []
+            },
+            {
+                "name" : "FEAST at Rieber",
+                "menu" : []
+            }
+        ],
+        "Dinner" : 
+        [
+            {
+                "name" : "Covel Dining",
+                "menu" : []
+            },
+            {
+                "name" : "Bruin Plate",
+                "menu" : []
+            },
+            {
+                "name" : "De Neve Dining",
+                "menu" : []
+            },
+            {
+                "name" : "FEAST at Rieber",
+                "menu" : []
+            }
+        ]
+    };
+
+    var tablesAsJson = tabletojson.convert(html);
+
+    for (var i = 1; i <= 5; i++)
+    {
+        var mealname;
+        var nameMap;
+
+        if (i == 1)
+        {
+            mealname = "Breakfast";
+            nameMap = 
+            {
+                "Bruin Plate" : 0,
+                "De Neve Dining" : 1
+            }
+        }
+        else 
+        {
+            if (i == 2 || i == 3)
+                mealname = "Lunch";
+            else if (i == 4 || i == 5)
+                mealname = "Dinner";
+            nameMap = 
+            {
+                "Covel Dining" : 0,
+                "Bruin Plate" : 1,
+                "De Neve Dining" : 2,
+                "FEAST at Rieber" : 3
+            }
+        }
+
+        var table = tablesAsJson[i];
+        var offset = 0;
+
+        var name1;
+        var name2;
+
+        if (i == 3 || i == 5)
+        {
+            offset = 1;
+            name1 = table[0]['0'];
+            name2 = table[0]['1'];
+        }
+        else
+        {
+            name1 = table[1]['0'];
+            name2 = table[1]['1'];
+        }
+        for (var j = (3-offset); j < table.length; j++)
+        {
+            var obj1 = 
+            {
+                "section_name" : "",
+                "items" : []
+            };
+
+            var obj2 = 
+            {
+                "section_name" : "",
+                "items" : []
+            };
+
+            var section1 = table[j]['0'];
+            var arr1 = section1.replace(/\t/g, '').split('\n');
+            obj1.section_name = arr1[0];
+            arr1.shift();
+            obj1.items = arr1;
+            response[mealname][nameMap[name1]].menu.push(obj1);
+            
+            var section2 = table[j]['1'];
+            var arr2 = section2.replace(/\t/g, '').split('\n');
+            obj2.section_name = arr2[0];
+            arr2.shift();
+            obj2.items = arr2;
+            response[mealname][nameMap[name2]].menu.push(obj2);
+        }
+    }
+    // send the response object to the /menus page
+    res.send(response);
+}
+
 function sendError(res, error) {
     //TODO: send JSON with the returned error message
     console.log('error')
@@ -124,5 +272,3 @@ function getDate(req, res) {
 
 // TODO: Have a job that runs every hour that refreshes all the menus
 // TODO: Store about a week's worth of menu info
-
-// test - jz
