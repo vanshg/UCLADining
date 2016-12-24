388e928564da6fb0f0e3f7e6d44e8f3335829363
Sahil Gandhi <sahilmgandhi@gmail.com>
2016-12-24 01:16:05 -0800

    Established connection to website. time to start cards and the parsing the JSON

--- BruinMenu/app/src/main/java/com/vanshgandhi/bruinmenu/HoursFragment.java
+++ BruinMenu/app/src/main/java/com/vanshgandhi/bruinmenu/HoursFragment.java
@@ -2,6 +2,7 @@ package com.vanshgandhi.bruinmenu;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,6 +22,12 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 /**
  * Created by Sahil on 12/12/2016.
@@ -33,9 +40,10 @@ public class HoursFragment extends Fragment {
 
     private TextView test;
 
-    public HoursFragment(){}
+    public HoursFragment() {
+    }
 
-    public static HoursFragment newInstance(){
+    public static HoursFragment newInstance() {
         return new HoursFragment();
     }
 
@@ -48,37 +56,52 @@ public class HoursFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
-        View rootview = inflater.inflate(R.layout.fragment_hours, container, false);
-        test = (TextView) rootview.findViewById(R.id.textView);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_hours, container, false);
+        test = (TextView) rootView.findViewById(R.id.textView);
         rightNow = mainActivity.getCurrentCal();
         String url = "https://salty-shelf-63361.herokuapp.com/hours";
-        String charset = "UTF-8";
-        SimpleDateFormat df = new SimpleDateFormat("MM/DD/yyyy", Locale.US);
-        String param1 = df.format(rightNow.getTime());
-        StringBuilder content = new StringBuilder();
+        //String charset = "UTF-8";
+        //SimpleDateFormat df = new SimpleDateFormat("MM/DD/yyyy", Locale.US);
+        //String param1 = df.format(rightNow.getTime());
 
-        try {
-            (HttpURLConnection) connection = (HttpURLConnection) new URL(url + "?" + param1).openConnection();
-            connection.setRequestProperty("Accept-Charset", charset);
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-
-            while ((line = bufferedReader.readLine()) != null)
-            {
-                content.append(line + "\n");
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("https://salty-shelf-63361.herokuapp.com/hours").get().build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("Test", "Oh noez! Error!!", e);
             }
-            bufferedReader.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
 
-        test.setText(content);
-
-        return rootview;
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String stringResponse = response.body().string();
+                response.body().close();
+                Log.v("Test", "Yay! Got response: " + stringResponse);
+            }
+        });
+        return rootView;
     }
 }
+
+//        StringBuilder content = new StringBuilder();
+//
+//        try {
+//            HttpURLConnection connection = (HttpURLConnection) (new URL(url)).openConnection();
+//            //connection.setRequestProperty("Accept-Charset", charset);
+//            connection.setReadTimeout(10000);
+//            connection.connect();
+//
+//            InputStream in = connection.getInputStream();
+//
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+//
+//            for (String line; (line = bufferedReader.readLine()) != null; ) {
+//                content.append(line);
+//            }
+//            in.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        test.setText(content.toString());
